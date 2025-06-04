b584b0310cf0e8fcfd795044482e0348376012d6
suzy <suzyp0223@gmail.com>
2025-06-05 01:29:55 +0900

    2,3dep 변동 수정중

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -25,38 +25,44 @@ const SideBar = ({ isOpen, onClose }: SidebarProps) => {
           ref={dropDownRef}
           className={clsx("absolute top-full left-20 w-54 bg-white shadow-md border z-50")}
         >
-          <ul className="p-4 border ">
+          <ul>
+            {/* <ul className="w-162"> */}
             {Category.map((category, index) => (
               <li
                 key={category.name}
-                className="relative group"
+                className="relative group w-40  h-12 min-h-[48px]"
                 onMouseEnter={() => setActiveIndex(index)}
                 onMouseLeave={() => setActiveIndex(null)}
               >
-                <Link href={category.pathName} className="hover:text-blue-500 block px-2 py-1">
+                <Link
+                  href={category.pathName}
+                  // className="hover:text-blue-500 block px-2 py-1 text-center"
+                  className="block text-center px-4 py-2 hover:text-blue-500"
+                >
                   {category.name}
                 </Link>
 
                 {/* 2단계: subcategories */}
                 {category.subcategories && activeIndex === index && (
-                  <ul className="absolute top-0 left-[100%] ml-4  bg-white border shadow-md min-w-[160px] z-50">
+                  <ul className="absolute top-0 left-full bg-white border shadow-md h-auto z-50">
                     {category.subcategories.map((sub) => (
-                      <li key={sub.name} className="group relative">
+                      <li key={sub.name} className="relative group w-40">
                         <Link
                           href={sub.pathName}
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap"
+                          // className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                         >
                           └ {sub.name}
                         </Link>
 
                         {/* 3단계: thirdSubcategories */}
                         {sub.thirdSubcategories && (
-                          <ul className="absolute top-0 left-full bg-white border shadow-md min-w-[160px] z-50">
+                          <ul className="absolute top-0 left-full bg-white border shadow-md h-auto z-50">
                             {sub.thirdSubcategories.map((third) => (
-                              <li key={third.name}>
+                              <li key={third.name} className="w-48">
                                 <Link
                                   href={third.pathName}
-                                  className="block whitespace-nowrap px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                  className="block whitespace-nowrap px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
                                 >
                                   &nbsp;&nbsp;&nbsp;&nbsp;↳ {third.name}
                                 </Link>
